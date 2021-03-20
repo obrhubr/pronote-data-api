2bca7747902c851d25b5604e20ffe88cd5d628d1
obrhubr <osmiumos74@gmail.com>
2021-03-20 15:31:00 +0100

    ➕ add cors dependency and use it

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //import pronote-api custom
 const pronote = require('./pronote-api-custom');
+//imprt cors, to enable accessing the pronote data
+const cors = require('cors');
 
 //create the app and use bodyparser
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
